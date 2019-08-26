f8660dbf3b3cc9d0d2d29b3acf576e4895a8cb5e
Juan Carmona <xjuanchovillax@gmail.com>
2019-08-25 21:36:04 -0600

    Add the board for the game
    I'm not very proficient in CSS and setting up the board for the game took me a very long time,

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Square = (props) => (
+  <button className='square'>{props.state}</button>
+)
+
+class Board extends React.Component {
+  renderSquare = (state = '') => {
+    return (<Square state={state}/>)
+  }
+
+  render() {
+    let sq1 = this.renderSquare('x');
+    let sq2 = this.renderSquare('o');
+    let sq3 = this.renderSquare();
+
+    let br1 = <div className='board-row'>{sq1}{sq3}{sq3}</div>
+    let br2 = <div className='board-row'>{sq3}{sq3}{sq3}</div>
+    let br3 = <div className='board-row'>{sq3}{sq3}{sq2}</div>
+
+    return (
+      <div className='board'>
+        {br1}
+        {br2}
+        {br3}
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        this will handle the logic of the game, bing bong
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Board/>
+      </div>
+    );
+  }
 }
 
 export default App;
