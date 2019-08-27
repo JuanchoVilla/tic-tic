64c3b57dcb75b6a342e70bf3dc84a869b0e2d368
Juan Carmona <xjuanchovillax@gmail.com>
2019-08-26 21:59:17 -0600

    Programmatically render the board
    Instead of having the board hard coded into the app, it now renders the squares programmatically based on user input

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import './App.css';
 
-const Square = (props) => (
-  <button className='square'>{props.state}</button>
-)
+class Square extends React.Component {
+  render () {
+    return (
+      <button className='square' 
+        onClick={this.props.onClick} >
+        {this.props.value}
+      </button>
+    )
+  }
+}
 
 class Board extends React.Component {
-  renderSquare = (state = '') => {
-    return (<Square state={state}/>)
+  state = {
+    squares: Array(9).fill(null)
+  }
+
+  handleClick = (i) => {
+    this.setState(prevState => ({
+      squares: [...prevState.squares, 'x']
+    })
+    )
+  }
+
+  renderSquare = (i) => {
+    return (
+    <Square value={this.state.squares}
+      onClick={() => this.handleClick(i)}
+    />)
   }
 
   render() {
-    let sq1 = this.renderSquare('x');
-    let sq2 = this.renderSquare('o');
-    let sq3 = this.renderSquare();
-
-    let br1 = <div className='board-row'>{sq1}{sq3}{sq3}</div>
-    let br2 = <div className='board-row'>{sq3}{sq3}{sq3}</div>
-    let br3 = <div className='board-row'>{sq3}{sq3}{sq2}</div>
 
     return (
       <div className='board'>
-        {br1}
-        {br2}
-        {br3}
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     )
   }
@@ -33,7 +59,7 @@ class Game extends React.Component {
   render() {
     return (
       <div>
-        this will handle the logic of the game, bing bong
+        <Board/>
       </div>
     )
   }
@@ -43,7 +69,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Board/>
+        <Game/>
       </div>
     );
   }
