2c5bc3f15353564ca960c97047d42bc22b31c040
Juan Carmona <xjuanchovillax@gmail.com>
2019-09-01 21:48:06 -0600

    Add redux to the app
    I added redux to manage the global state and make the recursive tic tac toe easier to manage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Game from './components/Game/Game';
+import store from './redux/store/store';
 
 class App extends React.Component {
+
   render() {
+
+    let counter = store;
+
     return (
       <div className="App">
         <Game/>
